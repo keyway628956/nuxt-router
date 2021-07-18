4bbaf8c00296e3f2e2cfc672516b55d4c8a16b7e
keyway <keyway628956@gmail.com>
2021-07-18 12:23:54 +0800

    6e8cf40

---
+++ _nuxt/0157f74.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{1306:function(e,n,t){"use strict";t.r(n);var c={},r=t(35),component=Object(r.a)(c,(function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)}),[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("\n    Subject\n  ")])])}],!1,null,null,null);n.default=component.exports}}]);
---
+++ _nuxt/44d07d5.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{1307:function(n,e,t){"use strict";t.r(e);var c={asyncData:function(n){return console.log(n.params.id),{id:n.params.id}}},r=t(35),component=Object(r.a)(c,(function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[t("h1",[n._v("Subject"+n._s(n.id))])])}),[],!1,null,null,null);e.default=component.exports}}]);
